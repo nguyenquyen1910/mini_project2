5881757fccc7336e95b5e6027d04b1d803db6201
nguyenquyen1910 <jrnguyen14@gmail.com>
2026-03-27 16:03:16 +0700

    feat: config app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+
+    implementation(libs.recyclerview)
+    implementation(libs.cardview)
+
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.livedata)
+
+    implementation(libs.glide)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
